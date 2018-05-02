1281db2e7f07fc299038018470aea8fee6ee4f39
andresum97 <andresum1309@gmail.com>
2018-05-02 08:43:49 -0600

    Permite saltar una sola vez, y ya estan los efectos para saltar

---
+++ Assets/Scripts/AreaEffector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEffector : MonoBehaviour {
+
+    float time=0f;
+    public GameObject gameObject;
+    public GameObject gameObject2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (time <= 5)
+        {
+            gameObject.SetActive(false);
+            gameObject2.SetActive(false);
+        }
+        else
+        if(time>5 && time<10)
+        {
+            gameObject.SetActive(true);
+            gameObject2.SetActive(true);
+        }
+        else
+        {
+            time = 0;
+        }
+        
+	}
+}
